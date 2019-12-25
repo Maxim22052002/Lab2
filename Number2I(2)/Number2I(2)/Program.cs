577d59dd0b42ee3b6f64ba62748aa16d86be2274
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 20:25:56 +0300

    T2

---
+++ Number2I(2)/Number2I(2)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Number2I_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i < 1000;i++)
+            {
+                int a = i / 100;
+                int c = i % 10;
+                int b = i / 10 % 10;
+                if (a == c)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
