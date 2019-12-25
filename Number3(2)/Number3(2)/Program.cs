95b2293b8f8025c5cb78db66fe8167f1f52dce5d
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 20:15:12 +0300

    T1

---
+++ Number3(2)/Number3(2)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Number3_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f1 = 1;
+            int f2 = 2;
+            int f0 = 1;
+            int counter = 0;
+            while (true)
+            {
+
+
+                f0 = f1;
+                f1 = f2;
+                f2 = f1 + f0;
+
+                if ((f1 + f2) >= 1000 && (f1 + f2) <= 9999)
+                {
+                    counter++;
+                }
+                else if (f1 + f2 > 9999)
+                {
+                    break;
+                }
+
+
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
