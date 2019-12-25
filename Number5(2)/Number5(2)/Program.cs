c0fe7ccf7c12f74868f913b3b58e98005a20312b
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 20:16:35 +0300

    T5

---
+++ Number5(2)/Number5(2)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Number5_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint N = Convert.ToUInt32(Console.ReadLine());
+            bool a = false;
+            for (uint x = 1; x < N; x++)
+            {
+                for (uint y = 1; y < N; y++)
+                {
+                    for (uint z = 1; z < N; z++)
+                    {
+                        if (Math.Pow(x, 3) + Math.Pow(y, 3) + Math.Pow(z, 3) == N)
+                        {
+                            Console.WriteLine(x +" , "+ y + " , "+z);
+                            a = true;
+                        }
+
+                    }
+                }
+            }
+            if (a == false)
+            {
+                Console.WriteLine("No such combinations!");
+            }
+            
+        }
+    }
+}
