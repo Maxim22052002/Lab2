65912c1d004ef9ed78e4ab298c566f3ca67755ea
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 20:14:30 +0300

    T2

---
+++ Number2(2)/Number2(2)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Number2_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            double total = 1;
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = 1;
+            for (int i = 3; i <= a; i += 2)
+            {
+                if (counter % 2 == 0)
+                {
+                    total -= b / i;
+
+                }
+                else if (counter % 2 == 1)
+                {
+                    total += b / i;
+                }
+                counter++;  
+            }
+            Console.WriteLine(total * 4);
+            Console.WriteLine(Math.PI);
+        }
+    }
+}
